7eeb095191490e491665ae1ec421b33185c4a13d
qarangy <qarangy@gmail.com>
2023-07-26 07:08:11 +0500

    update active showcase img on thumbnail click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const modifiers = {
+  imgThumbnailActive: 'img-showcase__thumbnail--active'
+};
+
+
+//SHOPPING CART MODAL
 const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
 const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
 
@@ -8,3 +14,23 @@ if (elSiteHeaderCartLink) {
     elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
   });
 }
+
+
+// IMAGE SHOWCASE
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__active-img');
+const elsImgThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+const elsImgThumbnailButton = document.querySelectorAll('.js-img-showcase__thumbnail-button');
+
+elsImgThumbnailButton.forEach(function (elButton) {
+  elButton.addEventListener('click', function () {
+    elsImgThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifiers.imgThumbnailActive);
+    })
+
+    elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+
+    elImgShowcaseActiveImg.src = elButton.dataset.showcaseImgActive;
+    elImgShowcaseActiveImg.srcset = `${elButton.dataset.showcaseImgActive} 1x, ${elButton.dataset.showcaseImgRetina} 2x`;
+  });
+
+})
