24e135508a85618207f3ce63a7075ac81edf9fc4
qarangy <qarangy@gmail.com>
2023-07-26 16:58:11 +0500

    add product page info

--- js/main.js
+++ js/main.js
@@ -129,3 +129,26 @@ if (elLightboxControlPrev) {
     elImgLightboxActiveImg.srcset = `${elNextActiveItem.children[0].dataset.showcaseImgActive} 1x, ${elNextActiveItem.children[0].dataset.showcaseImgRetina} 2x`;
   });
 }
+
+
+// PRODUCT COUNT
+const elProductQtyDecrementButton = document.querySelector('.js-product-quantity-decrement-button');
+const elProductQtyIncrementButton = document.querySelector('.js-product-quantity-increment-button');
+const elProductQty = document.querySelector('.product-info__quantity');
+
+
+if (elProductQtyIncrementButton) {
+  elProductQtyIncrementButton.addEventListener('click', function () {
+    elProductQty.textContent = parseInt(elProductQty.textContent, 10) + 1;
+  });
+}
+
+if (elProductQtyDecrementButton) {
+  elProductQtyDecrementButton.addEventListener('click', function () {
+    const qty = parseInt(elProductQty.textContent, 10);
+
+    if (qty > 0) {
+      elProductQty.textContent = qty - 1;
+    }
+  });
+}
